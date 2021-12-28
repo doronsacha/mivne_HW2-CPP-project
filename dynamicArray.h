a62a8ed25905ac854c7b7ab5f33adae326a34ace
doronsacha <slomovits@campus.technion.ac.il>
2021-12-29 00:41:12 +0200

    dynamic array look good

---
+++ dynamicArray.h
@@ -0,0 +1,99 @@
+#ifndef HW2MIVNE_DYNAMICARRAY_H
+#define HW2MIVNE_DYNAMICARRAY_H
+
+#include <iostream>
+using namespace  std;
+
+template<class T>
+class dynamicArray
+{
+private:
+    T* arr;
+    int size;
+    int number_of_element;
+    T null_element;// the default null element
+public:
+    dynamicArray(int i, T default_null)
+    {
+        if(i<=0)
+        {
+            cout << "cannot create an empty or negative input";
+            exit(0);
+        }
+        arr = new T[i](); //initialize all the values to be the null value;
+        size = i ;
+        number_of_element=0;
+        null_element= default_null;
+    };
+
+    ~dynamicArray()
+    {
+        delete []arr;
+        size = 0;
+        number_of_element=0;
+    };
+
+
+    T& operator[](int index)
+    {
+        if(index > size)
+        {
+            cout << " Array index out of bound !!!";
+            exit(0);
+        }
+        if(arr[index] == null_element)
+        {
+            return null_element;
+        }
+        return *(arr+index);
+    }
+    /*
+    dynamicArray(dynamicArray& other)
+    {
+        delete []arr;
+        arr = new T(other.size);
+        for(int i=0; i< other.size ; i++)
+        {
+            arr[i]=other[i];
+        }
+        size = other.size;
+        number_of_element= other.number_of_element;
+    };
+    */
+    void push(T insert)
+    {
+        if((size/2)<number_of_element)
+        {
+            resize();
+        }
+        arr[number_of_element] = insert;
+        number_of_element++;
+    }
+    void resize()
+    {
+        T* new_arr= new T[size*2]();
+        for(int i=0; i<size; i++)
+        {
+            new_arr[i]=arr[i];
+        }
+        delete []arr;
+        arr= new_arr;
+        size=size*2;
+    }
+
+    void set(int index, T value)
+    {
+        if(arr[index] != null_element)
+        {
+            return ;
+        }
+        if((size/2)<number_of_element)
+        {
+            resize();
+        }
+        number_of_element++;
+        arr[index] = value;
+    }
+};
+
+#endif //HW2MIVNE_DYNAMICARRAY_H
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+# include "dynamicArray.h"
+
+int main()
+{
+    dynamicArray<int> arr(4, -1);
+    arr.push(1);
+    arr.push(72);
+    arr.push(9);
+    arr.push(81);
+    arr.push(172);
+    arr.push(169);
+    arr.push(139);
+    arr.push(115);
+    arr.push(65);
+    arr.push(11);
+    arr.push(131);
+    arr.push(178);
+    arr.push(86);
+    arr.push(74);
+    arr.push(135);
+    arr.push(139);
+    for(int index=0; index<16; index++)
+    {
+        cout<< arr[index]<< endl;
+    }
+    return 0;
+}
